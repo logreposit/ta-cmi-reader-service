a54162d184f56f1dd639b877a1a74527baa88b5d
Dominic Miglar <dominic.miglar@netunix.at>
2022-01-16 13:53:54 +0100

    run.sh: removed -Xmx JVM param

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting application ..."
-exec java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ta-cmi-reader-service/app.jar
+exec java -Djava.security.egd=file:/dev/./urandom -jar /opt/logreposit/ta-cmi-reader-service/app.jar
