4f9ee29eb0e2e7fe3ed7e0694abfd074d6842cd3
Dominic Miglar <dominic.miglar@netunix.at>
2022-05-05 20:48:32 +0200

    LogrepositIngressDefinitionProvider.java: added 1st version of Device Ingress Definition (v2 API)

--- src/main/java/com/logreposit/ta/cmireaderservice/services/logreposit/LogrepositIngressDefinitionProvider.java
+++ src/main/java/com/logreposit/ta/cmireaderservice/services/logreposit/LogrepositIngressDefinitionProvider.java
@@ -1,12 +1,43 @@
 package com.logreposit.ta.cmireaderservice.services.logreposit;
 
+import com.logreposit.ta.cmireaderservice.services.logreposit.dtos.ingress.DataType;
+import com.logreposit.ta.cmireaderservice.services.logreposit.dtos.ingress.definition.FieldDefinition;
 import com.logreposit.ta.cmireaderservice.services.logreposit.dtos.ingress.definition.IngressDefinition;
+import com.logreposit.ta.cmireaderservice.services.logreposit.dtos.ingress.definition.MeasurementDefinition;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class LogrepositIngressDefinitionProvider
 {
     public IngressDefinition getIngressDefinition() {
-        return null; // TODO!
+        final var valueField = new FieldDefinition("value", DataType.FLOAT, "value");
+        final var tags = Set.of("number", "signal", "unit");
+
+        return new IngressDefinition(
+                List.of(
+                        new MeasurementDefinition(
+                                "input",
+                                tags,
+                                List.of(
+                                        valueField,
+                                        new FieldDefinition("ras_state", DataType.STRING, "RAS state")
+                                )
+                        ),
+                        new MeasurementDefinition(
+                                "output",
+                                tags,
+                                List.of(
+                                        valueField,
+                                        new FieldDefinition("state", DataType.INTEGER, "state")
+                                )
+                        ),
+                        new MeasurementDefinition("analog_logging", tags, List.of(valueField)),
+                        new MeasurementDefinition("digital_logging", tags, List.of(valueField))
+                        // TODO DoM: analog_network, digital_network for T.A. UVR1611
+                )
+        );
     }
 }
