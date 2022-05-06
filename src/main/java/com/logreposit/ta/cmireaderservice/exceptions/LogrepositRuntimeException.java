31e834a843883f8539b60094f20aba752e02e1a5
Dominic Miglar <dominic.miglar@netunix.at>
2022-05-06 19:39:32 +0200

    introduced LogrepositRuntimeException.java

---
+++ src/main/java/com/logreposit/ta/cmireaderservice/exceptions/LogrepositRuntimeException.java
@@ -0,0 +1,9 @@
+package com.logreposit.ta.cmireaderservice.exceptions;
+
+public class LogrepositRuntimeException extends RuntimeException
+{
+    public LogrepositRuntimeException(String message)
+    {
+        super(message);
+    }
+}
